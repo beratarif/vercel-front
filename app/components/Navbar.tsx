014fe6a024d3a9ccafdfbaaf0c3c92f6ac551399
Berat Arif Gönül <gonulberat1@gmail.com>
2026-02-18 14:37:53 +0300

    Mobile fix

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,21 +1,37 @@
-export default function Navbar(){
-  return(
+export default function Navbar() {
+  return (
     <nav className="navbar">
       <div className="nav-container">
-        <h2 className="nav-head">Berat.</h2>
+        <div className="desktop-nav">
+          <h2 className="nav-head">Berat.</h2>
 
-        <div className="nav-list">
-          <a href="#about" className="nav-link">
-            About
-          </a>
-          <a href="#projects" className="nav-link">
-            Projects
-          </a>
-          <a href="#contact" className="nav-link">
-            Contact
+          <div className="nav-list">
+            <a href="#about" className="nav-link">
+              About
             </a>
+            <a href="#projects" className="nav-link">
+              Projects
+            </a>
+            <a href="#contact" className="nav-link">
+              Contact
+            </a>
+          </div>
+        </div>
+        <div className="mobile-menu">
+          <h2 className="nav-head">Berat.</h2>
+          <div className="nav-list">
+            <a href="#about" className="nav-link">
+              About
+            </a>
+            <a href="#projects" className="nav-link">
+              Projects
+            </a>
+            <a href="#contact" className="nav-link">
+              Contact
+            </a>
+          </div>
         </div>
       </div>
     </nav>
-  )
+  );
 }
