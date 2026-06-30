3aeb779143a68aff0bbe04b7dfb4e684ea12efc6
Lunar Development <gonulberat1@gmail.com>
2026-06-30 22:54:14 +0300

    fix: form'a ratelimit, email optimizasyonu ve mesaj limiti eklendi

--- app/api/contact/route.tsx
+++ app/api/contact/route.tsx
@@ -1,19 +1,45 @@
 import { Resend } from "resend";
 import { NextResponse } from "next/server";
+import { ratelimit } from "@/app/lib/ratelimit";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: Request) {
   try {
+    const ip = req.headers.get("x-forwarded-for")?.split(",")[0] ?? "unknown";
+    const { success } = await ratelimit.limit(ip);
+    if (!success) {
+      return NextResponse.json(
+        {
+          error:
+            "Çok fazla istek gönderdiniz. Lütfen bir kaç dakika sonra tekrar deneyiniz. ",
+        },
+        { status: 429 },
+      );
+    }
     const { name, email, message } = await req.json();
 
     if (!name || !email || !message) {
       return NextResponse.json(
         { error: "Tüm alanları doldurmanız gerekiyor." },
-        { status: 400 }
+        { status: 400 },
       );
     }
 
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return NextResponse.json(
+        { error: "Geçerli bir e-posta adresi girin." },
+        { status: 400 },
+      );
+    }
+
+    if (message.length > 2000) {
+      return NextResponse.json(
+        { error: "Mesajınız çok uzun." },
+        { status: 400 },
+      );
+    }
     await resend.emails.send({
       from: "Portfolio Contact <onboarding@resend.dev>", // domain doğrulamadan önce bu kalsın
       to: process.env.CONTACT_EMAIL!,
@@ -27,7 +53,7 @@ export async function POST(req: Request) {
     console.error("Email gönderim hatası:", error);
     return NextResponse.json(
       { error: "Mesaj gönderilemedi, lütfen tekrar deneyin." },
-      { status: 500 }
+      { status: 500 },
     );
   }
 }
